da099e2c1ea98e495eb4c753f229ce37ce4b3c2e
Santiago Rueda Ortiz <srueda27@gmail.com>
2016-10-30 17:55:45 -0500

    Implement basic login with cookies

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,11 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :users
 
+  #Sessions
+
+  get '/login',      to: 'sessions#new'
+  post '/login',     to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,13 +22,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'form[action="/signup"]'
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
+      post signup_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
   end
 end
