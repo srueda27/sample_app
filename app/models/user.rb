7c31f7f1e018847e9646a579cd557b0bfd9cdb6e
Santiago Rueda Ortiz <srueda27@gmail.com>
2016-11-02 00:00:45 -0500

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  attr_accessor :remember_token
 
   before_save { email.downcase! }
   has_secure_password
@@ -9,10 +10,32 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false}
   validates :password, presence: true, length: { minimum:6 }
 
-  # Returns the hash digest of the given string.
-    def User.digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
+# Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+# Returns a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+# Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+# Forgets a user in the database for use in persistent sessions.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+# Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
